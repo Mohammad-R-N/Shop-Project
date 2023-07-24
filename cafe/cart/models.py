cb5d080c5b9d24649a4a327c1ff242720dad4b84
Mr-Coder <mohammad.ca@proton.me>
2023-07-24 12:35:04 -0400

    chore:cart app models updated

--- cafe/cart/models.py
+++ cafe/cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cafe.customer.models import Customer
+from cafe.staff.models import Staff
+
 
 # Create your models here.
+class Order(models.Model):
+    count=models.DecimalField(max_digits=3,decimal_places=0)
+    total_price=models.DecimalField(max_digits=9,decimal_places=0)
+    discount=models.DecimalField(max_digits=2,decimal_places=2)
+    order_time=models.DateTimeField(auto_now_add=True)
+    earned_point=models.DecimalField(max_digits=10,decimal_places=0)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    staff=models.ForeignKey(Staff,on_delete=models.CASCADE)
