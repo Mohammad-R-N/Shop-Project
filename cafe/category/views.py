6941dc43b16e039bf94d8de56b2d198b965030e0
Mr-Coder <mohammad.ca@proton.me>
2023-07-24 11:18:10 -0400

    chore:category page created

--- cafe/category/views.py
+++ cafe/category/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def category_page(request):
+    return render(request,"category/category.html")
--- cafe/main/urls.py
+++ cafe/main/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('',home_page,name="home-page"),
-    path("cart/",include("cart.urls"),name="cart-page"),
+    path("cart/",include("cart.urls")),
     path("menu/",include("menu.urls"),name="menu-page"),
     path("staff/",include("staff.urls"),name="staff-page"),
     path("customer/",include("customer.urls"),name="customer-page"),
-    path("category/",include("category.urls"),name="category-page")
+    path("category/",include("category.urls"))
 ]
