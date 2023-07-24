b1bd4dd21d762a37e3c3c45df20b6c48ea40e1e0
Mr-Coder <mohammad.ca@proton.me>
2023-07-24 12:40:27 -0400

    chore:Customer app models updated

--- cafe/customer/models.py
+++ cafe/customer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    phone=models.DecimalField(max_digits=11,decimal_places=0)
+    points=models.DecimalField(max_digits=10,decimal_places=0)
+
+    def __str__(self) -> str:
+        return self.phone
