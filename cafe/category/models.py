bb1aeee5697932e9e9386335b3215b88710427ce
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-07-30 10:18:40 +0330

    adding models for CART/CUSTOMER/MENU/STAFF/CATEGORY

--- cafe/category/models.py
+++ cafe/category/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.TextField(max_length=60)
+    photo = models.ImageField(upload_to='media/img')
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} category "
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
--- cafe/menu/models.py
+++ cafe/menu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from cafe.category.models import Category
+from cafe.cart.models import Cart
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    status = models.BooleanField(default=False)
+    photo = models.ImageField(upload_to='')
+    description = models.CharField(max_length=300)
+    point = models.IntegerField(default=0)
+    category_menu = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
+
+    def __str__(self):
+        return f"product : {self.name}  price ={self.price}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class OrderItem(models.Model):
+    menu = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
