cf0956729ee3b0f2f1adb2524c5348b9e290beb3
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-03 00:28:57 +0330

    fix: update url template

--- cafe/cart/urls.py
+++ cafe/cart/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('',cart_page,name="cart-page"),
+    path('',cart_page,name="cart"),
 ]
--- cafe/category/urls.py
+++ cafe/category/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('',category_page,name="category-page"),
+    path('',category,name="category"),
 ]
