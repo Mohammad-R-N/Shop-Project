db642994ff5a35dd7b354239427c60de1e1e4627
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-03 03:34:22 +0330

    fix: update cart view

--- cafe/cart/views.py
+++ cafe/cart/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from menu.models import Product
+
 # Create your views here.
 def cart_page(request):
-    if request.session.has_key('product'):
-        result = request.session['product']
-        pt = list()
-        for name in result:
-            product = Product.objects.get(name=name)
-            pt.append(product)
-
-        context = {
-            'product': pt
-        }
-        return render(request, "cart/cart.html", context)
-    else:
-        return render(request, "cart/cart.html")
+    if request.method == "GET":
+        if request.COOKIES.get('product') is not None:
+            result = request.COOKIES.get('product')
+            result = str(result).split('-')
+            pt = list()
+            for name in result:
+                product = Product.objects.get(name=name)
+                pt.append(product)
+            context = {
+                'product': pt
+            }
+            return render(request, "cart/cart.html", context)
+        else:
+            return render(request, "cart/cart.html")
+    if request.method == "POST":
+        if "done" in request.POST:
+            pt2 = list()
+            if request.COOKIES.get('product') is not None:
+                result = request.COOKIES.get('product')
+                result = str(result).split('-')
+                for name in result:
+                    product = Product.objects.get(name=name)
+                    pt2.append(product)
+            request.session['order'] = pt2
+            return redirect('menu')
+        else:
+            return redirect('home')
