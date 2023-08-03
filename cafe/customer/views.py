084ccc0d476b2ce8cf470746e6f127c11ef1af09
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-03 03:35:03 +0330

    fix: update customer view

--- cafe/customer/views.py
+++ cafe/customer/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def customer_page(request):
     return render(request,"customer/customer.html")
+
+def reservation(request):
+    return render(request, "customer/reserve.html")
