ac6c4087e8d80f990365e4a707274dad124dddf0
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-06 14:37:55 +0330

    improve product class

--- cafe/menu/models.py
+++ cafe/menu/models.py
@@ -4,21 +4,21 @@ from cart.models import Cart
 
 class Category(models.Model):
     name = models.TextField(max_length=60)
-    photo = models.ImageField(upload_to='media/img')
+    photo = models.ImageField(upload_to='media/')
 
     def __str__(self):
         return f"{self.name}"
 
-
 class Product(models.Model):
-    
+    IS_ACTIVE = "is_active"
+    NOT_ACTIVE = "not_active"
     STATUS_CHOICES = [
-    (True, 'active'),
-    (False, 'inactive'),
+    (IS_ACTIVE, 'active'),
+    (NOT_ACTIVE, 'inactive'),
     ]
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6,decimal_places=2)
-    status = models.CharField(max_length=5 , choices=STATUS_CHOICES, default="False")
+    status = models.CharField(max_length=10 , choices=STATUS_CHOICES, default=NOT_ACTIVE)
     photo = models.ImageField(upload_to='static/menu_photos')
     description = models.CharField(max_length=500)
     point = models.PositiveIntegerField(default=0)
@@ -30,8 +30,8 @@ class Product(models.Model):
 
 
 class OrderItem(models.Model):
-    menu = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Product, on_delete=models.PROTECT)
+    cart = models.ForeignKey(Cart, on_delete=models.PROTECT)
 
 
 
