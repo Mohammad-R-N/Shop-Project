b9e0cf3c624c15fecf5d925e5719c21e66029007
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-06 23:52:14 +0330

    fix : move users models from staff to users app

--- cafe/users/models.py
+++ cafe/users/models.py
@@ -19,3 +19,12 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.phone_number
+    
+class Users(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20, unique=True)
+    photo = models.ImageField(upload_to="media/users_photos")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
