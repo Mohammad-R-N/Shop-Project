4bc17d488eb2ce2323fcbba6616efd59e4008ca1
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-06 15:35:31 +0330

    improve and add some detail to cart class

--- cafe/cart/models.py
+++ cafe/cart/models.py
@@ -1,20 +1,7 @@
 from django.db import models
-from staff.models import Staff
+from staff.models import Users
 from customer.models import Customer
 
-class Cart(models.Model):
-    count = models.IntegerField()
-    total_price = models.IntegerField()
-    discount = models.IntegerField()
-    time = models.TimeField()
-    earned_point = models.IntegerField()
-    cart_customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
-    cart_staff = models.ForeignKey(Staff, on_delete=models.PROTECT, related_name='cart')
-
-    def __str__(self):
-        return f"{self.total_price} order "
-    
-    
 class Table(models.Model):
     Available = "Available"
     unavailable = "unavailable"
@@ -29,3 +16,18 @@ class Table(models.Model):
     status = models.CharField(max_length=10 , choices=STATUS_CHOICES, default=unavailable)    
 
     
+class Cart(models.Model):
+    total_price = models.DecimalField(max_digits=6,decimal_places=2)
+    discount = models.PositiveIntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+    total_quantity = models.PositiveIntegerField()
+    earned_point = models.PositiveIntegerField(default=0)
+    cart_customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    cart_staff = models.ForeignKey(Users, on_delete=models.PROTECT)
+    cart_table = models.ForeignKey(Table, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.total_price} order "
+    
+    
+
