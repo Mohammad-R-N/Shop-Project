72b7e039b769dacafb66aafce079cf1ebfb3aa33
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-06 14:16:27 +0330

    delete slug from category class

--- cafe/menu/models.py
+++ cafe/menu/models.py
@@ -5,7 +5,6 @@ from cart.models import Cart
 class Category(models.Model):
     name = models.TextField(max_length=60)
     photo = models.ImageField(upload_to='media/img')
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
