89e5e9d6e069e9cfc34df47397ee6080a4acc8bf
minatests <iminafattahi@gmail.com>
2023-08-06 21:25:52 +0330

    fix: default secret key was deleted. now the secret key from env is being read.

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-import environ
-env = environ.Env()
-environ.Env.read_env()
-SECRET_KEY = env("SECRET_KEY")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%vu8u&b(=$f*pq(4%@!(spr81)w@s!z(v@$^#+cq2a9+*rl1x'
+import environ
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
