6fd99270a71b24bdf3adf99e3a34b32a463a4172
minatests <iminafattahi@gmail.com>
2023-08-06 22:07:29 +0330

    feat: added register_user function in order for users to be able to sign up

--- cafe/users/views.py
+++ cafe/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
-# Create your views here.
+
+def register_user(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "register.html", {"form": form})
