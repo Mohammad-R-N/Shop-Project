64f7ecb4c4121b3739ad0e284673c8ff642a3b89
minatests <iminafattahi@gmail.com>
2023-08-06 21:35:56 +0330

    chor: added date_joined and user status fields to users model

--- cafe/users/models.py
+++ cafe/users/models.py
@@ -1,17 +1,19 @@
 from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
+from django.utils import timezone
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=30)
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_("first name"), max_length=20)
+    last_name = models.CharField(_("first name"), max_length=30)
     phone_number = models.CharField(_("phone number"), max_length=20, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    password_confirm = models.CharField(_("password"), max_length=128)
 
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = []
