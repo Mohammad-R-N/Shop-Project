01833edf722ba91b3d0c7d02ef36176349161059
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-06 12:39:26 +0330

    add category class to menu models

--- cafe/menu/models.py
+++ cafe/menu/models.py
@@ -21,3 +21,12 @@ class Product(models.Model):
 class OrderItem(models.Model):
     menu = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    name = models.TextField(max_length=60)
+    photo = models.ImageField(upload_to='media/img')
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
