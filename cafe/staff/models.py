95b6349c071448b393df41bbc560ad5d00989a0f
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-06 12:27:58 +0330

    delete unnecessary field from users class

--- cafe/staff/models.py
+++ cafe/staff/models.py
@@ -5,9 +5,7 @@ class Users(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
     phone_number = models.IntegerField()
-    address = models.CharField(max_length=150)
-    iban = models.IntegerField()
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
+    
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
