f3d2befe9e3c0b6cf77eb39702100919879c772e
nimagolkhanbanj <nimagolkhanban.ng@gmail.com>
2023-08-06 12:38:43 +0330

    remove category class from category models

--- cafe/category/models.py
+++ cafe/category/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 
 
-class Category(models.Model):
-    name = models.TextField(max_length=60)
-    photo = models.ImageField(upload_to='media/img')
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
 
-    def __str__(self):
-        return f"{self.name}"
 
 
