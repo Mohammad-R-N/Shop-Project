347582e00b608aa7a1434ecd5410a438227db473
minatests <iminafattahi@gmail.com>
2023-08-07 22:56:43 +0330

    refac: decided to user Regexvalidatior eventually

--- cafe/users/validation.py
+++ cafe/users/validation.py
@@ -1,5 +1,7 @@
-import re
+from django.core.validators import RegexValidator
 
-def phone_number_validator(phone_number):
-    pattern = r'^09\d{9}$'
-    return bool(re.match(pattern, phone_number))
+
+phone_regex = RegexValidator(
+    regex=r"^09\d{9}$",
+    message="Phone number must be entered in the format: '09XXXXXXXXX'."
+)
