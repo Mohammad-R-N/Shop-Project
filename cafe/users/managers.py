2df914632d584971d8faa54ea6c8f8897cc344be
minatests <iminafattahi@gmail.com>
2023-08-07 19:51:00 +0330

    fix: added =None to password in create superuser

--- cafe/users/managers.py
+++ cafe/users/managers.py
@@ -12,7 +12,7 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, phone_number, password, **extra_fields):
+    def create_superuser(self, phone_number, password=None, **extra_fields):
        
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
