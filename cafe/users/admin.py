d0534be92d8363fd0cf073d895ad6e2bc32e106a
minatests <iminafattahi@gmail.com>
2023-08-07 20:16:52 +0330

    feat: add fieldsets so that the admin panel for adding users is customized

--- cafe/users/admin.py
+++ cafe/users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import CustomUser
-from .models import Users
 
-admin.site.register(CustomUser)
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+
+
