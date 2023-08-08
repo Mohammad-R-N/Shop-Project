e5ef3fae030b57a7550a0c9211192a1a0bbf295d
MrSinxx <sinaleylaz@yahoo.com>
2023-08-09 00:21:02 +0330

    feat:url search logic

--- cafe/menu/urls.py
+++ cafe/menu/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('',menu_page,name="menu"),
-    path('',category,name="category"),
+    path("", menu_page, name="menu"),
+    path("", category, name="category"),
+    path("", search_products, name="search_products"),
 ]
