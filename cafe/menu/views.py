0be0f773a58b9cfa364b3bdfbacceef999c9f33d
MrSinxx <sinaleylaz@yahoo.com>
2023-08-09 00:20:36 +0330

    feat:search logic

--- cafe/menu/views.py
+++ cafe/menu/views.py
@@ -1,47 +1,40 @@
 from django.shortcuts import render, HttpResponse, redirect
-from . models import Product
-from . models import Category
-# Create your views here.
+from .models import Product
+from .models import Category
+
+
 def category():
     cat = Category.objects.all()
     return cat
-# Create your views here.
+
+
 def menu_page(request):
     if request.method == "POST":
-        if 'all' in request.POST:
+        if "all" in request.POST:
             cat = category()
             product = Product.objects.all()
-            context = {
-            "category": cat,
-            "product": product
-            }
-            return render(request,"menu/menu.html", context)
+            context = {"category": cat, "product": product}
+            return render(request, "menu/menu.html", context)
         else:
             cat = category()
             product = Product.objects.all()
             for cat_obj in cat:
                 if cat_obj.name in request.POST:
                     product_cat = Product.objects.filter(category_menu=cat_obj)
-                    context = {
-                        "category": cat,
-                        "product": product_cat
-                    }
-                    return render(request, 'menu/menu.html', context)
+                    context = {"category": cat, "product": product_cat}
+                    return render(request, "menu/menu.html", context)
             for pt in product:
                 if pt.name in request.POST:
                     result = pt.name
                     # del request.session['product']
-                    if request.COOKIES.get('product'):
-                        product1 = request.COOKIES.get('product')
-                        product1 += f"-{result}"  #produc1 = farzad-nima-mmd-sina
+                    if request.COOKIES.get("product"):
+                        product1 = request.COOKIES.get("product")
+                        product1 += f"-{result}"  # produc1 = farzad-nima-mmd-sina
                         cat = category()
                         product = Product.objects.all()
-                        context = {
-                            "category": cat,
-                            "product": product
-                        }
-                        res = render(request, 'menu/menu.html', context)
-                        res.set_cookie('product', product1)
+                        context = {"category": cat, "product": product}
+                        res = render(request, "menu/menu.html", context)
+                        res.set_cookie("product", product1)
                         return res
                         # res = render(request, 'menu/menu.html', context)
                         # res.delete_cookie('product')
@@ -49,18 +42,18 @@ def menu_page(request):
                     else:
                         cat = category()
                         product = Product.objects.all()
-                        context = {
-                            "category": cat,
-                            "product": product
-                        }
-                        response = render(request, 'menu/menu.html')
-                        response.set_cookie(key='product', value=result)
+                        context = {"category": cat, "product": product}
+                        response = render(request, "menu/menu.html")
+                        response.set_cookie(key="product", value=result)
                         return response
     else:
         cat = category()
         product = Product.objects.all()
-        context = {
-            "category": cat,
-            "product": product
-        }
-        return render(request,"menu/menu.html", context)
+        context = {"category": cat, "product": product}
+        return render(request, "menu/menu.html", context)
+
+
+def search_products(request):
+    query = request.GET.get("s", "")
+    results = Product.objects.filter(name__icontains=query)
+    return render(request, "search_results.html", {"results": results, "query": query})
