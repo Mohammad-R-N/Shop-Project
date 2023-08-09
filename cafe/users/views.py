4770c8062f9f79b2bd88b89db4d3386a15fe4e5f
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-09 23:38:32 +0330

    fix: update user view function to class base view

--- cafe/users/views.py
+++ cafe/users/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
-
+from django.views import View
 
 # Create your views here.
-def users_page(request):
-    return render(request,"users/staff.html")
-
-
-def register_user(request):
-    if request.method == "POST":
+class RegisterView(View):
+    def get(self, request):
+        form = CustomUserCreationForm()
+        return render(request, "register.html", {"form": form})
+    
+    def post(self, request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("login_user")
-    else:
-        form = CustomUserCreationForm()
-    return render(request, "register.html", {"form": form})
 
+class UserView(View):
+    def get(self, request):
+        return render(request,"users/staff.html")
 
+    def post(self, request):
+        pass
 
-def login_user(request):
-    if request.method == "POST":
+class LoginView(View):
+    def get(self, request):
+        form = CustomAuthenticationForm()
+        return render(request, "login.html", {"form": form})
+
+    def post(self, request):
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
             phone_number = form.cleaned_data.get("phone_number")
@@ -30,11 +36,11 @@ def login_user(request):
             if user is not None:
                 login(request, user)
                 return redirect("home")
-    else:
-        form = CustomAuthenticationForm()
-    return render(request, "login.html", {"form": form})
 
+class LogOutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect("home")
 
-def logout_user(request):
-    logout(request)
-    return redirect("home")
+    def post(self, request):
+        pass
