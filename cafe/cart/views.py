57b72ddffc67fdfc5b6d3add3b86f1344b53ffd5
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-09 23:26:37 +0330

    fix: update cart post method: save information in session

--- cafe/cart/views.py
+++ cafe/cart/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render, redirect
 from menu.models import Product
+from django.views import View
 
 # Create your views here.
-def cart_page(request):
-    if request.method == "GET":
+class CartView(View):
+    def get(self, request):
         if request.COOKIES.get('product') is not None:
             result = request.COOKIES.get('product')
             result = str(result).split('-')
-            pt = list()
+            # res_quantity = request.COOKIES.get('quantity')
+            # res_quantity = str(res_quantity).split('-') #'2-3-5-1-1-1'
+            # print(res_quantity  )
+            # quantity = list()
+            # for n in res_quantity:
+            #     print(n)
+            #     quantity.append(int(n))
+            product_list = list()
+            # quantity_list = list()
             for name in result:
                 product = Product.objects.get(name=name)
-                pt.append(product)
+                product_list.append(product)
             context = {
-                'product': pt
+                'product': product_list,
+                # 'quantity': quantity_list
             }
             return render(request, "cart/cart.html", context)
         else:
             return render(request, "cart/cart.html")
-    if request.method == "POST":
-        if "done" in request.POST:
-            pt2 = list()
-            if request.COOKIES.get('product') is not None:
-                result = request.COOKIES.get('product')
-                result = str(result).split('-')
-                for name in result:
-                    product = Product.objects.get(name=name)
-                    pt2.append(product)
-            # request.session['order'] = pt2
-            return redirect('menu')
-        else:
-            return redirect('home')
+        
