7347d278ec61a1f685f87f0fe1fa6700e469f777
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-09 23:28:31 +0330

    fix: update cart view: add reservation class

--- cafe/cart/views.py
+++ cafe/cart/views.py
@@ -28,3 +28,64 @@ class CartView(View):
         else:
             return render(request, "cart/cart.html")
         
+    def post(self, request):
+        if "done" in request.POST:
+            product_list = list()
+            cost = 0
+            if request.COOKIES.get('product') is not None:
+                # if request.COOKIES.get('quantity') is not None:
+                    result = request.COOKIES.get('product')
+                    result = str(result).split('-')
+                    # quantity = request.COOKIES.get('quantity')
+                    # quantity = str(quantity).split('-')
+                    for name in result:
+                        product = Product.objects.get(name=name)
+                        cost += product.price
+                        product_list.append(product.name)
+                    request.session['order'] = product_list
+            request.session['cost'] = str(cost)
+            return redirect('reservation')
+        # elif "remove" in request.POST:
+        #     pt_remove = request.POST['remove']
+        #     stop = 0
+        #     result = request.COOKIES.get('product')
+        #     for pt in result:
+        #         if pt_remove == pt:
+        #             pass
+        else:
+            return redirect('home')
+        
+class ReservationView(View):
+    def get(self, request):
+        context = {
+            'total': request.session['cost']
+        }
+        return render(request, "customer/reserve.html", context)
+    
+    def post(self, request):
+        cost = request.session['cost']
+        del request.session['cost']
+        table = request.POST['subject']
+        date = request.POST['date']
+        time = request.POST['time']
+        email = request.POST['2email']
+        if table == '1':
+            table = "2 person"
+        elif table == '2':
+            table = "5 person"
+        elif table == '3':
+            table = "6 person"
+
+        print(table)
+        print(date)
+        print(time)
+        print(email)
+        reserve_info = {
+            'table': table,
+            'date': date,
+            'time': time,
+            'email': email,
+            'cost': cost
+        }
+        request.session['reserve'] = reserve_info
+        return redirect('home')
