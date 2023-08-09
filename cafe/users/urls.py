def712b290630a707b9117906f284e39e9e97272
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-10 01:44:00 +0330

    fix: update urls pattern to class base view

--- cafe/users/urls.py
+++ cafe/users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.users_page, name="users"),
-    path("register/", views.register_user, name="register"),
-    path("login/", views.login_user, name="login"),
-    path("logout/", views.logout_user, name="logout_user"),
+    path("", views.UserView.as_view(), name="users"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogOutView.as_view(), name="logout_user"),
     path("staff/", views.StaffPanelView.as_view(), name="staff"),
     path('stafflogin/',views.StaffLogin.as_view(),name='staff-login')
 ]
