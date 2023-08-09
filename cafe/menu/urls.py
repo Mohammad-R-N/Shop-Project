9d664df5e9e9af8a74b6dbf8205984d68d1515c1
MrSinxx <sinaleylaz@yahoo.com>
2023-08-09 20:41:36 +0330

    feat:url to handle product popup

--- cafe/menu/urls.py
+++ cafe/menu/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", menu_page, name="menu"),
     path("", category, name="category"),
     path("search/", search_products, name="search_products"),
+    path("product_popup/<int:product_id>/", product_popup, name="product_popup"),
 ]
