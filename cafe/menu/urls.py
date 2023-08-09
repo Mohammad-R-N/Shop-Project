6123be4861d829ed17d781405c691aad62172f08
ranadanesh <ranadanesh.79@gmail.com>
2023-08-10 01:12:54 +0430

    fix: change the urls

--- cafe/menu/urls.py
+++ cafe/menu/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', MenuView.as_view(), name="menu"),
     path("search/", search_products, name="search_products"),
-    path("product_popup/<int:product_id>/", product_popup, name="product_popup"),
+    path("product_popup/<int:product_id>/", ProductPopup.as_view(), name="product_popup"),
 ]
