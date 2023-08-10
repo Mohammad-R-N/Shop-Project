b6a7f733365d74ccb860590e6bb702ba8f0c4c24
MrSinxx <sinaleylaz@yahoo.com>
2023-08-10 03:48:15 +0330

    feat: necessary urls for order details

--- cafe/menu/urls.py
+++ cafe/menu/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', MenuView.as_view(), name="menu"),
-    path("search/", SearchProducts.as_views(), name="search_products"),
-    path("product_popup/<int:product_id>/", ProductPopup.as_view(), name="product_popup"),
+    path("", MenuView.as_view(), name="menu"),
+    path("search/", SearchProducts.as_view(), name="search_products"),
+    path(
+        "product_popup/<int:product_id>/", ProductPopup.as_view(), name="product_popup"
+    ),
 ]
