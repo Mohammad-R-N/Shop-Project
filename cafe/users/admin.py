8ff13a41a237a661f85eabf84e4f51dd5e9719e1
MrSinxx <sinaleylaz@yahoo.com>
2023-08-10 03:50:01 +0330

    fix: removed unnecessary codes

--- cafe/users/admin.py
+++ cafe/users/admin.py
@@ -1,31 +1,39 @@
 from django.contrib import admin
-from .models import CustomUser,OtpCode
+from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
-
-
-@admin.register(OtpCode)
-class OtpCodeAdmin(admin.ModelAdmin):
-    list_display=('phone_number','code','created')
 
 
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('phone_number', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("phone_number", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('phone_number', 'password1', 'password2'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("phone_number", "password1", "password2"),
+            },
+        ),
     )
-    list_display = ('phone_number', 'first_name', 'last_name', 'is_staff')
-    search_fields = ('phone_number', 'first_name', 'last_name')
-    ordering = ('phone_number',)
+    list_display = ("phone_number", "first_name", "last_name", "is_staff")
+    search_fields = ("phone_number", "first_name", "last_name")
+    ordering = ("phone_number",)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
