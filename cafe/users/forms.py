764420d049cd6058640b6a7808e37b595d3f46c2
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-12 13:31:08 +0330

    fix: handled my login input in views and deleted the codes from this file

--- cafe/users/forms.py
+++ cafe/users/forms.py
@@ -3,7 +3,7 @@ from .models import CustomUser
 from django import forms
 import re
 from .validation import phone_number_validator
-from .models import PhoneNumberField
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -17,12 +17,8 @@ class CustomAuthenticationForm(AuthenticationForm):
         fields = ["phone_number", "password"]
 
 class StaffLoginForm(forms.Form):
-    # phone_number = PhoneNumberField(unique = True, max_length=19, validators=[phone_number_validator])
     phone_number=forms.CharField(label="PHONE NUMBER",max_length=19, validators=[phone_number_validator])
-    def clean_phone_number(self):
-        phone_number = self.cleaned_data['phone_number']
-        formatted_phone_number = re.sub(r'^0098|^\+98', '09', phone_number)
-        return formatted_phone_number
+
 
 class StaffOtpForm(forms.Form):
     code=forms.IntegerField()
