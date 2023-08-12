07b0634299121cfd97707d6735f44ea445e6f0e5
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-12 08:24:04 +0330

    fix: changed regex to accept phone numbers starting with 0098 and +98 as well

--- cafe/users/validation.py
+++ cafe/users/validation.py
@@ -2,6 +2,6 @@ from django.core.validators import RegexValidator
 
 
 phone_regex = RegexValidator(
-    regex=r"^09\d{9}$",
-    message="Phone number must be entered in the format: '09XXXXXXXXX'."
+    regex=r"^(09|00989|\+989)\d{9}$",
+    message="Phone number must be entered in the format: '09XXXXXXXXX', '00989XXXXXXXXX' or '+989XXXXXXXXX'."
 )
