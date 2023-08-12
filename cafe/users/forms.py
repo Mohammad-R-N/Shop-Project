10f87ce02911c25a577ef902e52ea9e6656066b6
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-12 13:00:42 +0330

    fix: corrcetd the field

--- cafe/users/forms.py
+++ cafe/users/forms.py
@@ -17,8 +17,8 @@ class CustomAuthenticationForm(AuthenticationForm):
         fields = ["phone_number", "password"]
 
 class StaffLoginForm(forms.Form):
-    phone_number = PhoneNumberField(unique = True, max_length=19, validators=[phone_number_validator])
-
+    # phone_number = PhoneNumberField(unique = True, max_length=19, validators=[phone_number_validator])
+    phone_number=forms.CharField(label="PHONE NUMBER",max_length=19, validators=[phone_number_validator])
     def clean_phone_number(self):
         phone_number = self.cleaned_data['phone_number']
         formatted_phone_number = re.sub(r'^0098|^\+98', '09', phone_number)
