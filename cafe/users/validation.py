bff55d8cf69fa3c701b54f3fcf559b2435ac8104
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-12 11:45:50 +0330

    refac: decided to write a function for validation instead of using Regexvalidator

--- cafe/users/validation.py
+++ cafe/users/validation.py
@@ -1,7 +1,13 @@
-from django.core.validators import RegexValidator
 
+import re
+from django.core.exceptions import ValidationError
 
-phone_regex = RegexValidator(
-    regex=r"^(09|00989|\+989)\d{9}$",
-    message="Phone number must be entered in the format: '09XXXXXXXXX', '00989XXXXXXXXX' or '+989XXXXXXXXX'."
-)
+def phone_number_validator(value):
+    """
+    Validates phone numbers in the 09XX, 00989XX, or +98XX format and replaces the +98 and 0098 parts with 0.
+    """
+    phone_regex = r'^(\+98|0098|0)?9\d{9}$'
+    if not re.match(phone_regex, value):
+        raise ValidationError("Phone number must be entered in the format: '09XXXXXXXXX', '00989XXXXXXXXX' or '+989XXXXXXXXX'.")
+    formatted_phone_number = re.sub(r'^\+98|^0098', '0', value)
+    return formatted_phone_number
