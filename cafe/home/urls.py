a998f6905915b0541dfc2481fac870f9d05c87f0
Mr-Coder <mohammad.ca@proton.me>
2023-08-12 18:22:32 -0400

    refactor:home page created instead of main

--- cafe/home/urls.py
+++ cafe/home/urls.py
@@ -1,12 +1,12 @@
-
 from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('',home_page,name="home"),
-    path("cart/",include("cart.urls")),
-    path("menu/",include("menu.urls")),
-    path("users/",include("users.urls")),
-    path("customer/",include("customer.urls")),
-    
+    path("", HomeView.as_view(), name="home"),
+    path("cart/", include("cart.urls")),
+    path("menu/", include("menu.urls")),
+    path("users/", include("users.urls")),
+    path("customer/", include("customer.urls")),
+    path("home", DefaultView.as_view(), name="main"),
+    path("logo/", LogoView.as_view(), name="logo"),
 ]
--- cafe/home/views.py
+++ cafe/home/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.views import View
+from .models import SiteConfig
+
 
 # Create your views here.
-def home_page(request):
-    return render(request,"home/home.html")
+class HomeView(View):
+    def get(self, request):
+        return render(request, "main/main.html")
+
+    def post(self, request):
+        pass
+
+
+class DefaultView(View):
+    template_name = "main/main.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class LogoView(View):
+    template_name = "base.html"
+
+    def get(self, request, *args, **kwargs):
+        site_config = SiteConfig.objects.first()
+        return render(request, self.template_name, {"site_config": site_config})
