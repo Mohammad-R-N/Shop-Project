c9409d9e1169c1c74a1af0661ca6bbdcba223447
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-13 16:41:45 +0330

    fix: add add order url to user url

--- cafe/users/urls.py
+++ cafe/users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.UserView.as_view(), name="users"),
     path("staff/", views.StaffPanelView.as_view(), name="staff"),
     path("edit_orders/", views.EditOrder.as_view(), name="edit_ord"),
+    path("add_orders/", views.AddOrder.as_view(), name="add_ord"),
     path("stafflogin/", views.StaffLogin.as_view(), name="login"),
     path("stafflogin/staff/otp/", views.CheckOtp.as_view(), name="check-otp"),
     path("logout/", views.LogOutView.as_view(), name="logout_user"),
