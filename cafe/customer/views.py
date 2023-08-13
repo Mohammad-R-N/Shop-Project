62d500d418f7461b4f480463d478b8ede876700a
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-13 20:13:31 +0330

    add customer history view

--- cafe/customer/views.py
+++ cafe/customer/views.py
@@ -8,3 +8,36 @@ class CustomerView(View):
         return render(request,"customer/customer.html")
     def post(self, request):
         pass
+
+class CustomerHistory(View):
+    template_name = "customer/history_login.html"
+    def get(self, request):
+        ord_history = request.session['reserve']
+        print(ord_history)
+        return render(request, self.template_name)
+    
+    def post(self, request):
+        if 'tel' in request.POST:
+            number = request.POST['tel']
+            ord_history = request.session['reserve']
+            print(ord_history)
+            result = list()
+
+            for ord in ord_history:
+                if ord['phone_number'] == number:
+                    data = dict()
+                    data['number'] = ord['phone_number']
+                    data['table'] = ord['table']
+                    data['total'] = ord['cost']
+                    data['date'] = ord['date']
+                    order_detail = list()
+                    order = ord["orders"]
+                    for ord in order:
+                        ords = ord.split('=')
+                        order_detail.append(ords)
+                    data['orders'] = order_detail
+                    data['quantity'] = len(order_detail)
+                    result.append(data)
+            
+            context = {"order": result}
+            return render(request, "customer/history.html", context)         
