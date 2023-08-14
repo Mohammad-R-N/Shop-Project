2c35fb770b3c8b8ed1e417e5628868884e252e86
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-14 12:44:22 +0330

    fix: update authentication add static method

--- cafe/users/authentication.py
+++ cafe/users/authentication.py
@@ -3,7 +3,8 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import redirect
 
 class CustomAuthBackend(BaseBackend):
-    def authenticate(self, request, phone_number=None, code=None, **kwargs):
+    @staticmethod
+    def authenticate(request, phone_number=None, code=None, **kwargs):
         User = get_user_model()
         try:
             user = User.objects.get(phone_number=phone_number)
