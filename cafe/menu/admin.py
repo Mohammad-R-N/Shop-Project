122f1cf731bf53bc45df14cefe97b479395e6293
ranadanesh <ranadanesh.79@gmail.com>
2023-08-16 23:52:01 +0430

    feat: add new feature to product admin

--- cafe/menu/admin.py
+++ cafe/menu/admin.py
@@ -3,15 +3,26 @@ from .models import Category
 from .models import Product
 # Register your models here.
 
+
+
+
 class ProductAdmin(admin.ModelAdmin):
+
+    actions = ['make_inactive']
+
+    @admin.action(description='Mark selected product as inactive')
+    def make_inactive(modeladmin, request, queryset):
+        queryset.update(status='not_active')
+
     search_fields = ['name', 'description']
-    list_display = ['name', 'price', 'category_menu']
+    list_display = ['name', 'price', 'category_menu', 'status']
     list_filter = ['status']
 
 
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ['name']
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
