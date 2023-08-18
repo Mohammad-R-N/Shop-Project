1b011dc00ae093c1f60aa55b9c54419238d72aea
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-18 15:25:55 +0330

    add utils function for staff edit ord

--- cafe/users/utils.py
+++ cafe/users/utils.py
@@ -81,4 +81,50 @@ class StaffPanel:
                 messages.success(request, "Accepted successfully!", "success")
         return None
     
-    def 
+    def get_ord_for_edit(request, model):
+        cart_edit_id = request.session["edit_id"]
+        cart = model.objects.all()
+        item = list()
+        cart_list = list()
+
+        for cart_obj in cart:
+            if cart_obj.id == int(cart_edit_id):
+                items = OrderItem.objects.filter(cart=cart_obj)
+                cart_list.append(cart_obj)
+                item.append(items)
+        return item, cart_list[0].customer_number
+
+    def remove_ord(request, model):
+        cart_edit_id = request.session["edit_id"]
+        order_item_id = request.POST["remove"]
+        cart = model.objects.all()
+        item_list = list()
+
+        for cart_obj in cart:
+            if cart_obj.id == int(cart_edit_id):
+                items = OrderItem.objects.filter(cart=cart_obj)
+                item_list.append(items[0])
+
+        for item in item_list:
+            if item.id == int(order_item_id):
+                OrderItem.objects.get(id=int(order_item_id)).delete()
+                messages.success(request, "Deleted successfully!", "warning")
+                return True
+        return False
+    
+    def save_new_quantity(request, model):
+        order_items = model.objects.all()
+
+        for ord in order_items:
+            if str(ord.id) in request.POST:
+                new_quantity = request.POST[f"{ord.id}"]
+                old_quantity = ord.quantity
+                total_quantity = int(old_quantity) - int(new_quantity)
+                ord.quantity = int(new_quantity)
+                ord.cart.total_price = ord.price * int(new_quantity)
+                ord.cart.total_quantity = abs(total_quantity)
+                ord.save()
+                return True
+        return False
+    
+    
