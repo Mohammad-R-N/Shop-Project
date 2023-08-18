5ab1a72d6300d5e702a78fef8b53006835575664
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-18 16:36:40 +0330

    delete extra func in home view

--- cafe/home/views.py
+++ cafe/home/views.py
@@ -7,17 +7,12 @@ from .models import SiteConfig
 class HomeView(View):
     def get(self, request):
         return render(request, "main/main.html")
-
-    def post(self, request):
-        pass
-
-
+    
 class DefaultView(View):
     template_name = "main/main.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-
 
 class LogoView(View):
     template_name = "base.html"
