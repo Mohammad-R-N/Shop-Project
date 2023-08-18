d28721f2cd1e796e2a7cc331fe28f22366638c77
minatests <iminafattahi@gmail.com>
2023-08-18 15:46:01 +0330

    feat: max length tests

--- cafe/cart/tests/test_models.py
+++ cafe/cart/tests/test_models.py
@@ -15,3 +15,16 @@ class TestTableModel(TestCase):
     def test_table_status(self):
         table2 = Table.objects.create(table_name= 'Table 2', status = 'Available')
         self.assertEquals(table2.status, 'Available')
+
+    def test_table_update_status(self):
+        self.table1.status = 'unavailable'
+        self.table1.save()
+        self.assertEqual(self.table1.status, 'unavailable')
+
+    def test_table_status_max_length(self):
+        max_length = Table._meta.get_field('status').max_length
+        self.assertEqual(max_length, 20)
+
+    def test_table_name_max_length(self):
+        max_length = Table._meta.get_field('table_name').max_length
+        self.assertEqual(max_length, 100)
