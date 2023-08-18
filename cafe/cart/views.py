c1b49443bb6d8a0eb8b85d9b65a53af827a4bdaa
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-18 16:18:24 +0330

    refactor: update cart view with solid

--- cafe/cart/views.py
+++ cafe/cart/views.py
@@ -3,76 +3,22 @@ from menu.models import Product
 from cart.models import Table, Cart, OrderItem
 from django.views import View
 from django.contrib import messages
+from .utils import ProductOption
 
 class CartView(View):
-    cost = 0
     def get(self, request):
-        product_list = list()
-
         if request.COOKIES.get('product') is not None:
-            result = request.COOKIES.get('product')
-            result = str(result).split('-')
-            result.pop(0)
-            
-            for name in result:
-                pt_name = name.split('=')
-                product = Product.objects.get(name=pt_name[0])
-                data = dict()
-                data['product_name'] = product.name
-                data['product_photo'] = product.photo.url
-                data['product_price'] = product.price
-                data['product_quantity'] = pt_name[1]
-                data['total'] = int(pt_name[1]) * product.price
-                product_list.append(data)
-
-            for pt in product_list:
-                self.cost += pt['total']
-            
-            request.session['total'] = str(self.cost)
-            context = {
-                'product': product_list,
-                'sub_total': self.cost,
-            }
+            result = ProductOption.show_product(request, Product)
+            context = {'product': result[1], 'sub_total': result[0],}
             return render(request, "cart/cart.html", context)
-        
         else:
             return render(request, "cart/cart.html")
         
     def post(self, request):
         if "remove" in request.POST:
-            remove = request.POST['remove']
-            result = request.COOKIES.get('product')
-            result = str(result).split('-')
-            result.pop(0)
-            new_cookie = ''
-
-            for name in result:
-                pt_name = name.split('=')
-                if pt_name[0] == remove:
-                    pass
-                else:
-                    new_cookie += f"-{pt_name[0]}={pt_name[1]}"
-            try:
-                response = redirect("cart")
-                response.set_cookie(key='product', value=new_cookie)
-                return response
-            except:
-                response = redirect("cart")
-                response.set_cookie(key='product', value='')
-                return response
-        
+            ProductOption.remove_from_shop_cart(request)
         elif "done" in request.POST:
-            if request.COOKIES.get('product') is not None:
-                if len(request.COOKIES.get('product')) > 1:
-                    result = request.COOKIES.get('product')
-                    result = str(result).split('-')
-                    result.pop(0)
-                    request.session['order'] = result
-                    request.session['cost'] = request.session['total']
-                    del request.session['total']
-                    return redirect('reservation')
-                else:
-                    return redirect('cart')
+            ProductOption.accept_shop_cart(request)
         else:
             return redirect('home')
         
