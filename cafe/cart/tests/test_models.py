b5ead5a3b230f2d049b9e03548a39b2714904a6f
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-19 10:28:34 +0330

    test: cart status update

--- cafe/cart/tests/test_models.py
+++ cafe/cart/tests/test_models.py
@@ -70,3 +70,8 @@ class TestCartModel(TestCase):
     def test_cart_customer_number_max_length(self):
         max_length = Cart._meta.get_field('customer_number').max_length
         self.assertEqual(max_length, 14)
+
+    def test_cart_update_status(self):
+        self.cart1.status = 'a'
+        self.cart1.save()
+        self.assertEqual(self.cart1.status, 'a')
