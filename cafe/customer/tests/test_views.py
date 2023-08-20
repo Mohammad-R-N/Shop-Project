92eb4ff915594f6202c10e6ef913e4bcc2027542
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-20 09:05:54 +0330

    test: customer view GET method reponse and template

--- cafe/customer/tests/test_views.py
+++ cafe/customer/tests/test_views.py
@@ -8,4 +8,9 @@ class TestCustomerView(TestCase):
     def setUp(self):
         self.client= Client()
         self.customer_url = reverse('customer-page')
+   
+    def test_customer_view_GET(self):
+        response = self.client.get(self.customer_url)
 
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'customer/customer.html')
