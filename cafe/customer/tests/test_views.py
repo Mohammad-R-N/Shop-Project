82d482c51cd9f5220e1915c051aed86e3630fa83
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-20 09:10:09 +0330

    test: customer history view GET method reponse and template

--- cafe/customer/tests/test_views.py
+++ cafe/customer/tests/test_views.py
@@ -8,9 +8,17 @@ class TestCustomerView(TestCase):
     def setUp(self):
         self.client= Client()
         self.customer_url = reverse('customer-page')
+        self.customer_history_url = reverse('customer-history')
    
     def test_customer_view_GET(self):
         response = self.client.get(self.customer_url)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'customer/customer.html')
+
+    def test_customer_hostory_GET(self):
+        response = self.client.get(self.customer_history_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'customer/history.html')
+
