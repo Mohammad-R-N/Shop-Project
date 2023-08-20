33f17e7f004c1d6ae050798086ff3bb23d2c831c
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-20 09:57:15 +0330

    test: home view GET status code and template

--- cafe/home/tests/test_views.py
+++ cafe/home/tests/test_views.py
@@ -10,4 +10,8 @@ class TestHomerView(TestCase):
         self.client= Client()
         self.home_url = reverse('home')
 
+    def test_home_view_GET(self):
+        response = self.client.get(self.home_url)
 
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/main.html')
