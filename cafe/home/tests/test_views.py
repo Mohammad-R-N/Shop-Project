d093b8e03ee51e53f9ebda4c59c68ec4b6cb1ea7
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-20 09:44:10 +0330

    test: added setUp for home view tests

---
+++ cafe/home/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from home.models import SiteConfig
+
+
+
+class TestHomerView(TestCase):
+
+    def setUp(self):
+        self.client= Client()
+        self.home_url = reverse('main/main.html')
+
+
+    
