f8403f8b4fe4dde4aae402bbd0a16bb5108660f6
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-20 08:33:20 +0330

    test: phone number validation

--- cafe/customer/tests/test_models.py
+++ cafe/customer/tests/test_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from customer.models import *
+from decimal import Decimal
+from django.core.exceptions import ValidationError
 
 class TestCustomerModel(TestCase):
     def setUp(self) -> None:
@@ -9,3 +11,14 @@ class TestCustomerModel(TestCase):
     def test_customer_str(self):
         self.assertEquals(str(self.customer1.phone_number), '09123456789')
 
+    def test_Customer_phone_number_max_length(self):
+        max_length = Customer._meta.get_field('phone_number').max_length
+        self.assertEqual(max_length, 14)
+    
+    
+
+    def test_customer_number_validation(self):
+        customer2 = Customer(phone_number = "0912", order = 0, point = 0 )
+        with self.assertRaises(ValidationError) as err:
+            customer2.full_clean() 
+        self.assertEqual(err.exception.message_dict['phone_number'], ["Phone number must be entered in the format: '09XXXXXXXXX', '00989XXXXXXXXX' or '+989XXXXXXXXX'."])
