90060d2b9831d9c1a9665f81cecd5bdae369b520
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-20 08:29:47 +0330

    fix: imported gettext_lazy

--- cafe/customer/models.py
+++ cafe/customer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.validation import phone_number_validator
 from users.models import PhoneNumberField 
+from django.utils.translation import gettext_lazy as _
 
 class Customer(models.Model):
     phone_number = PhoneNumberField(_("phone number"), max_length=14, validators=[phone_number_validator], unique = True)
