76870dc77722405205aeb2006f2743ab07ea7588
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-20 08:24:06 +0330

    test: phon enumber max length

--- cafe/customer/tests/test_models.py
+++ cafe/customer/tests/test_models.py
@@ -6,4 +6,10 @@ class TestCustomerModel(TestCase):
         self.customer1 = Customer.objects.create( phone_number = "09123456789", order = 0, point = 0)
 
 
+    def test_customer_str(self):
+        self.assertEquals(str(self.customer1.phone_number), '09123456789')
+
+    def test_Customer_phone_number_max_length(self):
+        max_length = Customer._meta.get_field('phone_number').max_length
+        self.assertEqual(max_length, 14)
     
