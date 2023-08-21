3505e618f73157bc5da44e3b3dd538a28bbf92d2
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-21 08:43:37 +0330

    test: search products view test class and its func

--- cafe/menu/tests/test_views.py
+++ cafe/menu/tests/test_views.py
@@ -9,12 +9,15 @@ class TestMenuView(TestCase):
     def setUp(self):
         self.client= Client()
         self.menu_url = reverse('menu')
-        self.search_product_url = reverse('search_products')
-
-
 
     def test_menu_view_GET(self):
         response = self.client.get(self.menu_url)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'menu/menu.html')
+
+
+class TestSearchProducts(TestCase):
+    def setUp(self):
+        self.client= Client() 
+        self.search_product_url = reverse('search_products')
