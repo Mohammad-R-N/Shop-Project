a728728c070063ae5536e0bc8dbe53464d26e4a7
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-21 08:24:39 +0330

    test: category str

--- cafe/menu/tests/test_models.py
+++ cafe/menu/tests/test_models.py
@@ -5,3 +5,6 @@ from menu.models import Category, Product
 class TestCategoryModel(TestCase):
     def setUp(self):
          self.category = Category.objects.create( name = "category 1")
+
+    def test_category_str(self):
+        self.assertEquals(str(self.category), 'category 1')
