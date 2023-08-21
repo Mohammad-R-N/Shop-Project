1bd4f27bfa7af61bac8161102ebb7ba1580b963f
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-21 08:51:27 +0330

    refactor: better indenting

--- cafe/menu/tests/test_models.py
+++ cafe/menu/tests/test_models.py
@@ -5,15 +5,15 @@ from menu.models import Category, Product
 
 class TestCategoryModel(TestCase):
     def setUp(self):
-         self.category = Category.objects.create( name = "category 1")
+        self.category = Category.objects.create( name = "category 1")
 
     def test_category_str(self):
         self.assertEquals(str(self.category), 'category 1')
 
 class TestProductModel(TestCase):
     def setUp(self):
-         self.category = Category.objects.create( name = "category 1")
-         self.product = Product.objects.create( name = "product 1", price = 10.12 , category_menu = self.category, status = 'active')
+        self.category = Category.objects.create( name = "category 1")
+        self.product = Product.objects.create( name = "product 1", price = 10.12 , category_menu = self.category, status = 'active')
 
     def test_category_str(self):
         self.assertEquals(str(self.product), 'product : product 1  price =10.12')
