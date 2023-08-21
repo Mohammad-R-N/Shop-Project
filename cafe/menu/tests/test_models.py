0fdf45e0c5a3ec5a72c9d972560284e80697ab85
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-21 08:31:03 +0330

    test: product str

--- cafe/menu/tests/test_models.py
+++ cafe/menu/tests/test_models.py
@@ -15,3 +15,5 @@ class TestProductModel(TestCase):
          self.category = Category.objects.create( name = "category 1")
          self.product = Product.objects.create( name = "product 1", price = 10.12 , category_menu = self.category, status = 'active')
 
+    def test_category_str(self):
+        self.assertEquals(str(self.product), 'product : product 1  price =10.12')
