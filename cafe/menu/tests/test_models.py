8a4c953c79f431be451eaac65a938c52a2c73cd9
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-21 08:30:47 +0330

    test: product test setup func

--- cafe/menu/tests/test_models.py
+++ cafe/menu/tests/test_models.py
@@ -2,9 +2,16 @@ from django.test import TestCase
 from menu.models import Category, Product
 
 
+
 class TestCategoryModel(TestCase):
     def setUp(self):
          self.category = Category.objects.create( name = "category 1")
 
     def test_category_str(self):
         self.assertEquals(str(self.category), 'category 1')
+
+class TestProductModel(TestCase):
+    def setUp(self):
+         self.category = Category.objects.create( name = "category 1")
+         self.product = Product.objects.create( name = "product 1", price = 10.12 , category_menu = self.category, status = 'active')
+
