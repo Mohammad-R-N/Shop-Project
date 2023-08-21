ce14918dcdf0222fc25060b9a31b1813fdd15629
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-21 11:56:57 +0330

    test: custom user manager create super user

--- cafe/users/tests/test_managers.py
+++ cafe/users/tests/test_managers.py
@@ -5,6 +5,7 @@ class CustomUserManagerTests(TestCase):
 
     def setUp(self) -> None:
         self.user = CustomUser.objects.create_user(phone_number="09123456789", password='pass')
+        self.user2 = CustomUser.objects.create_superuser(phone_number="09191234567", password='pass')
 
 
     def test_create_user(self):
@@ -14,3 +15,8 @@ class CustomUserManagerTests(TestCase):
         self.assertFalse(self.user.is_staff)
         self.assertFalse(self.user.is_superuser)
         self.assertTrue(self.user.is_active)
+
+    def test_create_super_user(self):
+        
+        self.assertEqual(self.user2.phone_number, '09191234567')
+        self.assertTrue(self.user2.check_password('pass'))
