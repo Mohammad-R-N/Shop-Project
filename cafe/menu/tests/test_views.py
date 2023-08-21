4eb8a1bbbc5a472063c5609959cf1b14ba9eb4b0
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-21 09:17:30 +0330

    test: search product query context

--- cafe/menu/tests/test_views.py
+++ cafe/menu/tests/test_views.py
@@ -21,12 +21,26 @@ class TestSearchProducts(TestCase):
     def setUp(self):
         self.client= Client() 
         self.search_product_url = reverse('search_products')
+        self.category = Category.objects.create( name = "category 1", photo = 'test.png')
+        self.product1 = Product.objects.create( name = "product 1", price = 20.12 , category_menu = self.category,photo = 'product.png', status = 'active', )
+        self.product2 = Product.objects.create( name = "product 2", price = 10.12 , category_menu = self.category,photo = 'product.png', status = 'active', )
 
     def test_search_product_GET(self):
         response = self.client.get(self.search_product_url)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'menu/search_results.html')
+        
+
+    def test_search_product_with_query_context(self):
+        query1 = "product 1"
+        response = self.client.get(self.search_product_url, {"query": query1 })
+        
+        self.assertContains(response, query1)
+        self.assertIn(self.product1, response.context["results"])
+        self.assertNotIn(self.product2, response.context["results"])
+
+
 
 
 class TestProductPopup(TestCase):
