f4d8eb4393226317617c5642d2db6090ad65fa21
Mina Fatahi <M.fatahi@DB.LOCAL>
2023-08-30 11:50:11 +0330

    tset: cart view with cookie

--- cafe/cart/tests/test_views.py
+++ cafe/cart/tests/test_views.py
@@ -13,7 +13,7 @@ class TestCartView(TestCase):
         self.client= Client()
         self.cart_url = reverse('cart')
         self.category = Category.objects.create(name = 'food')
-        self.product = Product.objects.create(name='Pizza', price=Decimal('10.00'), category_menu = self.category)
+        self.product = Product.objects.create(name='Pizza', price=10.00, category_menu = self.category)
         self.cart = Cart.objects.create(total_price=Decimal('0.00'), total_quantity=0,  customer_number = "09191234567", cart_table=Table.objects.create(table_name = "test table"),)
     
    
@@ -29,6 +29,11 @@ class TestCartView(TestCase):
         response = self.client.get(self.cart_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'cart/cart.html') 
+        self.assertTrue('product' in self.client.cookies)
+        self.assertEqual(self.client.cookies['product'].value, 'Pizza')
+        self.assertEqual(str(self.product), 'product : Pizza  price =10.0')
+        
+
 
 
 
@@ -81,3 +86,5 @@ class TestReservationView(TestCase):
     def setUp(self):
         self.client= Client()
         self.ord_detail_url = reverse('reservation')
+
+
